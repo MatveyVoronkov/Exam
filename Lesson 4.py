05360e0321d5055ffafca3f850a8fd7a5d1c7129
admin <voronkov.matv@gmail.com>
2023-06-02 22:09:27 +0300

    Lesson 4

---
+++ Lesson 4.py
@@ -0,0 +1,14 @@
+class Student:
+    def __init__(self):
+        print("Hello!")
+student=Student()
+
+class Parent:
+    def __init__(self):
+        print("Hello!")
+parent=Parent()
+
+class Grandparent:
+    def __init__(self):
+        print("Hello!")
+grandparent=Grandparent()
