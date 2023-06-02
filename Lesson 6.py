30d83222e6f2421a7ab9523a7d69dd3dd2b30ed0
admin <voronkov.matv@gmail.com>
2023-06-02 16:33:14 +0300

    Lesson_6

--- Lesson 6.py
+++ Lesson 6.py
@@ -2,13 +2,13 @@ def divider(a, b):
    try:
        return a / b
    except ZeroDivisionError:
-       print("Ви не можете ділити на нуль!")
+       print("Zero Division Error")
        return 0
    except TypeError:
-       print("Ви не можете ділити нечислові значення!")
+       print("Type Error")
        return 0
    except ValueError:
-       print("Ви не можете ділити нечислові значення!")
+       print("Value Error")
        return 0
 data = {10: 2, 2: 5, "123": 4, 18: 0, 8: 4}
 result = []
